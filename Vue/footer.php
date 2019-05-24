df91595237a990fc7240b1e2a704208c7e823dd0
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2019-05-24 16:27:00 +0200

    boostratp en local

--- Vue/footer.php
+++ Vue/footer.php
@@ -16,8 +16,8 @@
 </div> <!-- #global -->
 
 <!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="./Vendor/jquery/jquery.min.js"></script>
+<script src="./Vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 
 </body>
--- Vue/header.php
+++ Vue/header.php
@@ -12,7 +12,9 @@
     <head>
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+        <!-- Bootstrap core CSS -->
+        <!-- Bootstrap core CSS -->
+        <link href="./Vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
         <meta charset="UTF-8" />
         <base href="<?= $racineWeb ?>" >
