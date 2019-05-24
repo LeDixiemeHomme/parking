13cd205962e69735802e993774fa9a1018f40d1a
LeDixiemeHomme <benoit.valleea@hotmail.fr>
2019-05-24 16:38:56 +0200

    ajustement pour serveur

--- Controleur/ControleurCompte.php
+++ Controleur/ControleurCompte.php
@@ -158,8 +158,11 @@ class ControleurCompte extends ControleurSecurise
         $placeattente = $this->reservation->getReservationsMouvant($resaUser['date_resa']);
 
         $resaUAttente = $this->reservation->getReservationEnAttenteByUser($_SESSION['id_u']);
-        if (count($this->reservation->getReservationsMouvant($resaUAttente['date_resa'])))
+        if (isset($this->reservation->getReservationsMouvant($resaUAttente['date_resa'])))
         $rang = count($this->reservation->getReservationsMouvant($resaUAttente['date_resa']));
+        else
+            $rang = 0;
+
 
         $this->genererVue(array('users' => $users, 'placeL' => $placeL, 'placeO' => $placeO, 'resaU' => $resaUser, 'rang' => $rang));
     }
